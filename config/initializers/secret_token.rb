b53c5804fc699a95d060bff271c742d51f0d5ed8
D Tucker <danielle@tuckerlabs.com>
2013-08-15 16:31:22 -0700

    Initial blog app before berkshelf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '20be2dfde26711b5a21962c5cf09cecea90521cbd5429673ea59038e0d59f7afae0b9e4cf3435e253102acf6057ae9210475c980aba097ffa36990cfd200b399'
